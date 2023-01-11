8fa07609f7f5ece6f2664918dfa9c0768a5a2e22
황의혁 <weihyuk@naver.com>
2023-01-11 18:21:19 +0900

    login localstorage에 jwt token받기

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,51 +1,13 @@
 import React from 'react'
-import Button from '../components/ui/Button';
 import { useState } from 'react';
 import axios from 'axios'
-import { useEffect } from 'react';
-import { login } from '../api/firebase';
+
 
 export default function Login() {
   const [Id, setId] = useState("");
   const [Password, setPassword] = useState("");
-   //axios
-  const[posts,setPosts] = useState([]);
 
-  useEffect(() => {
-      axios({
-          method:'GET',
-          url:'https://jsonplaceholder.typicode.com/posts'
-      }).then(response => setPosts(response.data))
-      
-        //jsonplaceholder fake api
-        //await, async promise
-        //axios로 GET으로 데이터 받아오기 예시
-    },[])
-
-    //참고 자료 : https://ajdahrdl.tistory.com/136
-    var sendData = JSON.stringify({
-        "username" : {Id},
-        "password" : {Password}
-    });
-    useEffect(() => {
-    axios({
-        method:"POST",
-        url : '/axiosTest', //url 다시
-        data: sendData,
-        // header에서 JSON 타입의 데이터라는 것을 명시
-        headers : {'Content-type' : 'application/json'}
-    }).then((res)=>{
-        alert("성공 로그인 되었습니다.");
-        // API로 부터 받은 데이터 출력하기
-        console.log(res.data);
-    }).catch(error=>{
-        // console.log("실패");
-        
-    })
-    },[])
-    //onSubmitHandler가 들어가야되나 ?
-
-    const onIdHandler = (e) =>{
+    const onIdHandler = (e) => {
       setId(e.currentTarget.value);
   }
 
@@ -57,9 +19,61 @@ export default function Login() {
       e.preventDefault();//page가 refresh 방지
   }
 
+  axios.defaults.withCredentials = true;
+
+  const onLogin = (e) => {
+    e.preventDefault();
+
+    //json.strin
+    const data = JSON.stringify({
+      "username" : Id,
+      "password" : Password,
+    });
+
+    axios.post('http://3.38.35.43:8080/login',data,{
+      headers :{
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then(response => {
+      console.log(response.headers.get("Authorization")) 
+      // const { accessToken } = response.data;
+      //localStorage에 
+      window.localStorage.setItem("Login", response.headers.get("Authorization"))
+      
+      //getItem => 변수 가져오기 
+      console.log("=======로그인 성공=========")
+  
+      const val = response.headers.get("Authorization")
+      
+      // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
+      // axios.defaults.headers.common['Authorization'] = val;
+      // accessToken을 localStorage, cookie 등에 저장하지 않는다!      
+      //main
+      document.location.href = '/'
+    }).catch(error => {
+      // ... 에러 처리
+      console.log("에러")
+      console.log(error)
+    });
+
+    
+  }
+  
+
+    
+  
+  //로그인이 
   return (
     <div className='flex justify-center items-center w-full h-screen' >
         <form className='flex flex-col' onChange={onSubmitHandler}>
+            
+            {/* <ul>
+            {posts.map(post => (
+              <li key={post.id}>{post.title}</li>
+            ))}
+            </ul> */}
+
             <label>ID</label>
             <input 
             type="text"
@@ -73,28 +87,20 @@ export default function Login() {
             <label>PASSWARD</label>
             <input
             type="text"
-            placeeholder='PASSWORD'
+            placeeholder="PASSWORD"
             className='rounded-3xl'
             value={Password}
             onChange={onPasswordHandler}
             >
             </input>
             <br />
-            <button
-             >
-                    login
-            </button>
             <div>
-            <br></br>
-            <hr></hr>
-            <p>
-            posts의 길이가 100이 나오면 성공 : {posts.length}
-            {/* posts의 길이가 data 만큼 나오면 되는거임 */}
-            </p>
+            <button onClick={onLogin}> 로그인 버튼!!</button>
+            <div></div>
             </div>
         </form>    
-      
-       
     </div>
   )
 }
+
+

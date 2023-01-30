4c84bf5bd648dd8b048450711185eadaa9c91055
HyeongjinKim98 <201721157@sangmyung.kr>
2023-01-30 13:34:00 +0900

    s3저장소 이미지 불러오기 구현중

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import { getProducts } from '../api/firebase';
 import ProductCard from './ProductCard';
 import useProducts from './hooks/useProducts';
 import styled from "styled-components";
+import { LoadProductsAll } from '../api/api';
+import axios from 'axios'
+
+const baseURL = "http://3.38.35.43:8080"
 
 const CategoriesContainer = styled.div`
     height : 80px;
@@ -37,11 +41,21 @@ export default function Products() {
     const handleCategoryClick = (props) =>{
       setCategory(props)
     }
-    const {
-      isLoading,
-      error,
-      data: products,
-    } = useQuery(['products'], getProducts);
+    const [products,setProducts] = useState();
+    useEffect(() => {
+      axios({
+        method: "get",
+        url: baseURL +"/shop",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      }).then((response) => {
+        console.log(response.data)
+        setProducts(response.data)
+      })
+        .catch((error) => console.log(error))
+    }, [])
+  
     return (
       <>
         <CategoriesContainer>
@@ -51,8 +65,8 @@ export default function Products() {
           <Category onClick={() => handleCategoryClick("pants")}>PANTS</Category>
           <Category onClick={() => handleCategoryClick("shoes")}>SHOES</Category>
         </CategoriesContainer>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
+        {/* {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>} */}
 
         <ul className='grid grid-cols-1 md:grid-cols-3 lg-grid-cols-4 gap-4 p-4'>
           {products &&

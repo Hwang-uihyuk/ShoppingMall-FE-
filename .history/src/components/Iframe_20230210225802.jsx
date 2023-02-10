8740f71add2ddd9b580b17c78ba8bfb05651e72d
Hwang-uihyuk <weihyuk39@gmail.com>
2023-02-11 01:09:08 +0900

    chore deploy

---
+++ .history/src/components/Iframe_20230210225802.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Iframe() {
+  return (
+    <div className='w-100% h-100%'>
+        <iframe  src="https://www.youtube.com/embed/pT1ZjkN73Q4?autoplay=0&mute=1 
+        frameborder = 0"  title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; picture-in-picture; " ></iframe>
+    </div>
+  )
+}

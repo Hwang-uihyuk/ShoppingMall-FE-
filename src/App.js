0ab7fdc92b2785536398eaa99a9357170f0fd51f
HyeongjinKim98 <201721157@sangmyung.kr>
2023-02-17 17:27:23 +0900

    Deploy Test

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { Outlet } from 'react-router-dom';
-import { AuthContextProvier } from './components/context/AuthContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Footer from './components/Footer';
 
 const queryClient = new QueryClient();
 function App() {
   return (
-    <AuthContextProvier>
     <QueryClientProvider client={queryClient}>
-        <Navbar />
-        
+        <Navbar /> 
         <Outlet />
         <Footer/>
     </QueryClientProvider>
-    </AuthContextProvier>
   );
 }
 

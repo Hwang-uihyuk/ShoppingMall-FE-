33a3b6dc706a5048aa5d5eedfbeb1369fa3c8581
HyeongjinKim98 <201721157@sangmyung.kr>
2023-03-05 16:43:33 +0900

    fix : 메인화면 Carousel 수정

--- src/components/SimpleSlider.jsx
+++ src/components/SimpleSlider.jsx
@@ -8,11 +8,51 @@ import styled from "styled-components";
 const CarouselContainer= styled.div`
   width : 100%;
   display: flex;
+  align-items: center;
+  flex-direction: column;
+  position : relative;
 `
 const BannerImg = styled.img`
   width : 100%;
   margin: auto;
+  opacity: 1;
 `
+const Label1 = styled.label`
+  width: 100%;
+  display: flex;
+  z-index: 100;
+  color : white;
+  font-size: 6rem;
+  font-weight: 700;
+  margin: auto;
+  position: absolute;
+  top : 47%;
+  left : 50%;
+  transform: translate(-50%,-50%);
+  justify-content: center;
+`
+const BannerBtn = styled.button`
+  background-color: #252525;
+  color : white;
+  width : 15%;
+  height : 8%;
+  z-index: 100;
+  font-size: 1.5rem;
+  font-weight: 700;
+  margin: auto;
+  position: absolute;
+  bottom : 30%;
+  left : 50%;
+  text-align: center;
+  transform: translate(-50%,-50%);
+`
+const Carousel = ({link,text})=>(
+  <CarouselContainer>
+    <BannerImg src = {link}/>
+    <Label1>{text}</Label1>
+    <BannerBtn>GO SHOP</BannerBtn>
+  </CarouselContainer>
+);
 export default function SimpleSlider() {
   //https://poew.tistory.com/707
     var settings = {
@@ -28,18 +68,10 @@ export default function SimpleSlider() {
       <>
         <div className='pt-10'>
           <Slider {...settings}>
-            <CarouselContainer>
-              <BannerImg src="/images/carousel_sample_1.jpg"/>
-            </CarouselContainer>
-            <CarouselContainer>
-              <BannerImg src="/images/carousel_sample_2.jpg"/>
-            </CarouselContainer>
-            <CarouselContainer>
-              <BannerImg src="/images/carousel_sample_3.jpg"/>
-            </CarouselContainer>
-            <CarouselContainer>
-              <BannerImg src="/images/carousel_sample_4.jpg"/>
-            </CarouselContainer>
+            <Carousel link = "/images/carousel_sample_1.jpg" text ="NEW ARRIVAL"/>
+            <Carousel link = "/images/carousel_sample_2.jpg" text ="NEW ARRIVAL"/>
+            <Carousel link = "/images/carousel_sample_3.jpg" text ="NEW ARRIVAL"/>
+            <Carousel link = "/images/carousel_sample_4.jpg" text ="NEW ARRIVAL"/>
               {/* <section className='h-96 bg-yellow-900 relative'>
                 <div className='w-full h-full bg-cover bg-banner opacity-70' />
                   <div className='absolute w-full h-full top-32 text-center text-gray-50 drop-shadow-2xl'>

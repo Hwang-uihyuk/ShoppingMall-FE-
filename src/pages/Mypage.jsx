839125da700eda8719970be05a0a043342a1244e
HyeongjinKim98 <201721157@sangmyung.kr>
2023-03-06 13:28:12 +0900

    fix : 주문목록 코드 수정

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -560,7 +560,7 @@ const handleUpgradeAuth = (e) => {
             </div>
 
             <div className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                <Link to ="/mypage/ =orderlist">주문목록</Link>
+                <Link to ="/mypage/orderlist">주문목록</Link>
             </div>
             {window.localStorage.getItem('ID') === 'hwang' ?
             <button 

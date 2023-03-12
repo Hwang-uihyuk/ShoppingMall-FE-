c820c0601094901f7d61da590e29256388fd3dd3
HyeongjinKim98 <201721157@sangmyung.kr>
2023-03-12 17:22:19 +0900

    style : 상품 상세페이지 디자인 변경

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -141,27 +141,40 @@ console.log(detaildata)
         {/* <img className='w-full px-4 basis-7/12' src={detaildata.imgkey} alt={title} /> */}
         <img className=" w-150 px-4 basis-3/12" src={detaildata.imgKey}></img>
         <div className='w-full basis-5/12 flex flex-col p-4'>
-          <p className='text-xl mt-4 text-gray-700'>{detaildata.category}</p>
+          <p className='text-l mt-4 text-gray-700 uppercase'>{detaildata.category}</p>
           <p className='text-2xl font-bold mt-3 text-gray-700'>{detaildata.name}</p>
           <h2 className='text-3xl font-bold py-2'>{title}</h2>
-          <p className='text-2xl font-bold py-2  border-b border-gray-400'>
-            ₩{price}
-          </p>
+          <div className='flex justify-between border-b'>
+            <p className='text-2xl font-bold py-2 border-gray-400'>
+              {price.toLocaleString('to-kr')}원
+            </p>
+            <div className='flex'>
+              <div className='flex'>
+                {!detaildata.check_favorite &&
+                  <AiOutlineHeart size="38" onClick={handleAddLike} className=' p-1 text-lg hover:focus'> </AiOutlineHeart>}
+              </div>
+              <div>
+                {detaildata.check_favorite &&
+                  <AiFillHeart size="38" onClick={handleDeleteLike} className=' p-1 text-lg'> 좋아요 해제{detaildata.favorite} </AiFillHeart>
+                }
+              </div>
+
+              <div className='pt-1.5'>{detaildata.favorite}</div>
+            </div>
+          </div>
+          <div className='pt-1.5'>{detaildata.favorite}</div>
           <p className='py-4 text-lg opacity-50'>{detaildata.description}</p>
 
           {/* 상품 재고  */}
           {detaildata &&
             detaildata.stock <= 10 ? <div className='font-bold'> 품절임박 남은 재고 수 : {detaildata.stock} </div> : ''}
           <div className='flex items-center'>
-            <label className='text-brand font-bold' htmlFor='select'>
+            <label className='text-black font-bold' htmlFor='select'>
               옵션:
             </label>
-            
-          
-
             <select
               id='select'
-              className='p-2 m-4 flex-1 border-2 border-dashed border-brand outline-none'
+              className='p-1 m-4 flex-3 border-2 border-black outline-none'
               onChange={handleSelect}
               value={selected}
             >
@@ -175,56 +188,30 @@ console.log(detaildata)
                 ))}
             </select>
 
-{/* 내가만든 select */}
-            {/* <select>
-            {detaildata.size.split("").map((val,idx) => (
-                  <option key={idx}> {val}</option>))}
-            </select> */}
+            {/* 내가만든 select */}
+              {/* <select>
+              {detaildata.size.split("").map((val,idx) => (
+                    <option key={idx}> {val}</option>))}
+              </select> */}
           </div>
           {success && <p className='my-2'>✅{success}</p>}
-          <span className='flex'>
 
+          <span className='flex justify-end'>
+            {/* <Button text='장바구니에 추가' onClick={handleAddCart}/> */}
           
-          {/* <Button text='장바구니에 추가' onClick={handleAddCart}/> */}
-
-          {selected === "사이즈를 선택하세요." ? 
-          <button className =" text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" disabled onClick={handleAddCart}>Select the size(cart)</button>
-          :
-          <button type="submit"  onClick={handleAddCart} className=" text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-            Add to Cart</button>
-        }
-          
-
-
-          {selected === "사이즈를 선택하세요." ? 
-          <button className=" text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" disabled>Select the size(buy)</button>
-          
-          :
-          !window.localStorage.getItem('Login') ? 
-          navigate('/login')
-          
-          
-          :
-          <Link to ='/order' state = {{product : detaildata, from : 'details', size :selected,}}>
-            <button className=" text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Buy</button>
-          </Link>
+            {selected === "사이즈를 선택하세요." ? 
+              <button className=" text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" disabled>Select the size</button>
+            :
+            !window.localStorage.getItem('Login') ? 
+            navigate('/login')
+            :
+            <>
+              <button type="submit"  onClick={handleAddCart} className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 mr-3">ADD TO CART</button>
+              <Link to ='/order' state = {{product : detaildata, from : 'details', size :selected,}}>
+                <button className=" text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">BUY</button>
+              </Link>
+            </>
 }
-
-<div className='flex'>
-          <div className='flex'>
-          {!detaildata.check_favorite && 
-          <AiOutlineHeart size = "38" onClick={handleAddLike} className=' p-1 text-lg hover:focus'> </AiOutlineHeart>}
-          </div>
-
-          <div>
-          {detaildata.check_favorite && 
-          <AiFillHeart size = "38" onClick={handleDeleteLike} className=' p-1 text-lg'> 좋아요 해제{detaildata.favorite} </AiFillHeart>
-          }
-          </div>
-
-          <div className='pt-1.5'>{detaildata.favorite}</div>
-          
-</div>     
           </span>
         </div>
       </section>
